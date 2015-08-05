7408127d0b93c06dc5a5d359f1eb2a7a7b5590e6
KaterinaKovalchuk <kovalchyk.katya@gmail.com>
2015-08-05 18:01:55 +0300

    JS

---
+++ JS/Lesson1/Task1/js_1.js
@@ -0,0 +1,5 @@
+var name; // variable declaration.
+var developer; // variable declaration.
+var name="Sergey"; //name initialization
+var developer=name; //copy data
+alert(developer); //display result
---
+++ JS/Lesson1/Task2/js_2.js
@@ -0,0 +1,2 @@
+var yourName=prompt("Как тебя зовут?",'');//variable declaration. data from user
+alert("Тебя зовут " + yourName+".")//display result
